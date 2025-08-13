7965291365d1bd0d5b1f93590534ee71e764f4ab
Souvik-R <souvikr212@gmail.com>
2025-08-14 00:27:05 +0530

    called the youtuve api and fetch data

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+
+const GOOGLE_API_KEY = "AIzaSyBWZ200-jvDgnY4Xm_yB_ZO_ZFqpvJIJUU";
+
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+ GOOGLE_API_KEY
