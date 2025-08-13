00fc4603aabb9f667869fc510162c4fa5d7cc3cc
Souvik-R <souvikr212@gmail.com>
2025-08-13 23:45:00 +0530

    button list fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({name}) => {
     return (
         <div>
-            <button className='px-5 py-2 m-2 bg-gray-200 rounded-lg cursor-pointer'>All</button>
+            <button className='px-5 py-2 m-2 bg-gray-200 rounded-lg cursor-pointer'>{name}</button>
         </div>
     )
 }
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Button from './Button';
 
+const list = ["All", "Gaming", "Movies", "Entertainment", "Kapil Sharma", "News", "Funny", "Cricket", "Accessories"]
+
+
 const ButtonList = () => {
     return (
         <div className='flex'>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
-            <Button />
+            {list.map((name, index) => (
+                <Button key={index} name={name} />
+            ))}
         </div>
+        
     )
 }
 
